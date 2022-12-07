23889b2b06c64395eb84fe6c3852ca09331f8e9e
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2022-12-07 16:32:09 -0500

    Connect home and contact routes to the navbar

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
 
   return (
     <div id ="content">
-      <Header />
       <Router>
+      <Header />
       <div className = "container">
         <Routes>
           <Route path="/" element={<Home />}></Route>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import './header.css';
+import { Link } from 'react-router-dom';
+
 
 
 function Header (){
     return(
         <header>
             <div className="header-container">
-            <navbar>
-                <a>Home</a>
-                <a>About</a>
-                <a>Work</a>
-                <a>Contact</a>
-                <a id="menu-bars">☰</a>
-            </navbar>
+                <Link to="/">
+                <div className="logo">
+                    <h1>KM.</h1>
+                </div>
+                </Link>
+                <navbar>
+                    <Link to="/home">
+                        <a>Home</a>
+                    </Link>
+                    <a>About</a>
+                    <a>Work</a>
+                    <Link to="/contact">
+                        <a>Contact</a>
+                    </Link>
+                    <a id="menu-bars">☰</a>
+                </navbar>
             </div>
         </header>
     )

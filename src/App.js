0ba51bd1ae296f2ea5e30324ebbe1753841820f6
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2022-12-07 15:25:41 -0500

    Add beginning route functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header'
 import Contact from './components/Contact';
@@ -9,10 +11,15 @@ function App() {
   return (
     <div id ="content">
       <Header />
+      <Router>
       <div className = "container">
-        <Home />
-        <Contact />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
       </div>
+      </Router>
       <Footer/>
     </div>
   );

a43f4e832cd0d5d24e68ea53f6d7a2787ca15dbb
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2022-12-08 08:11:10 -0500

    Add functionality to open and close nav

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import './header.css';
 import { Link } from 'react-router-dom';
+import close from "../Images/close.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faX } from '@fortawesome/free-solid-svg-icons'
 
 
+
+function openNav(){
+    document.getElementById("sideNav").style.width = "100%";
+    console.log("Opening!")
+};
+
+function closeNav(){
+    document.getElementById("sideNav").style.width = "0";
+    console.log("Closing!")
+};
 
 function Header (){
     return(
@@ -28,8 +39,23 @@ function Header (){
                     <Link to="/contact">
                         <a>Contact</a>
                     </Link>
-                    <p id="menu">&#9776;</p>
+                    <p id="menu" onClick={openNav}>&#9776;</p>
                 </navbar>
+            </div>
+            <div className="sidenav" id="sideNav">
+                <img src={close} className="closebtn" onClick={closeNav}></img>
+                    <Link to="/home">
+                        <a>Home</a>
+                    </Link>
+                    <Link to="/about">
+                        <a>About</a>
+                    </Link>
+                    <Link to="/work">
+                        <a>Work</a>
+                    </Link>
+                    <Link to="/contact">
+                        <a>Contact</a>
+                    </Link>
             </div>
         </header>
     )
